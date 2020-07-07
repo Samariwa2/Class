4dd2e2dc13c0ae275ccb078fca7a3ec1cbc2a61e
Samariwa2 <63638144+Samariwa2@users.noreply.github.com>
2020-07-07 03:38:43 -0400

    My class exercises

---
+++ 4.0 Data Objects/Konekta.php
@@ -0,0 +1,53 @@
+<?php
+
+#namespace Models;
+
+class Konekta{
+
+  #will hold our connection resource
+  public $konn;
+
+  #the configuration string
+  private $config;
+
+  public function __construct(){
+    #get our configuration and save it in $config
+    $this->getConfig();
+
+    #new mysqli connection : OOP
+    $this->konn = new mysqli($this->config->host,
+    $this->config->username,
+    $this->config->password,
+    $this->config->db);
+
+    if ($this->konn->connect_errno) {
+      echo "Failed to connect to MySQL: " . $this->konn->connect_error;
+      exit();
+    }
+
+  }
+
+  public function getConfig(){
+
+<<<<<<< HEAD
+    $config_string = file_get_contents('config.json');
+=======
+    $config_relative_path ='config.json';
+    $config_absolute_path = __DIR__.'/config.json';
+
+    $config_string = file_get_contents($config_absolute_path);
+>>>>>>> b757b72b917bbdfe69457a5970bfc1c298be9122
+
+    $config_object = json_decode($config_string);
+
+    $this->config = $config_object;
+  }
+
+}
+<<<<<<< HEAD
+
+$kn = new Konekta();
+
+var_dump($kn->konn);
+=======
+>>>>>>> b757b72b917bbdfe69457a5970bfc1c298be9122
---
+++ 4.0 Data Objects/README.md
@@ -0,0 +1,14 @@
+# Data Objects 🙌
+
+<img src='../resources/images/dobjects.jpg' alt="oop">
+
+## Aim 🏹
+
+- Reading config values from file
+- Connecting to Database using MySQLi
+- CRUD
+<<<<<<< HEAD
+- (....)
+=======
+- ORMs
+>>>>>>> b757b72b917bbdfe69457a5970bfc1c298be9122
